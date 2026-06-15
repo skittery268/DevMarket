d2f88cd11a0c1c4109f80de6ec402ba5ebdbc6e6
skittery268 <sabadzidzikashvili6@gmail.com>
2026-06-15 22:36:43 +0400

    Fix bug

--- server/configs/passport.config.js
+++ server/configs/passport.config.js
@@ -13,7 +13,7 @@ passport.use(
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 
-            callbackURL: "/api/v1/auth/google/callback",
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
